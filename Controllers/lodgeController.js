34e2635aae7e2ccbb6e13a812c09f92308a22e9b
karimerrebai <karimerrebai@gmail.com>
2022-11-14 17:46:19 +0100

    lodge changing

--- Controllers/lodgeController.js
+++ Controllers/lodgeController.js
@@ -2,15 +2,32 @@ const Lodge = require('../Models/lodge')
 const Place = require('../Models/place')
 
 addLodge = async (req, res) => {
-  const lodge = new Lodge(req.body)
-  await lodge.save()
-  await Place.findByIdAndUpdate({
-    _id: req.body.place
-  }, { $push: { lodges: lodge } })
-  res.status(200).json({
-    data: lodge,
-    msg: 'lodge created successfully'
-  })
+
+  // req.body["galleries"] = req.file.filename //lire une seul image file;
+  try {
+
+
+    req.body['galleries'] =
+      req.files.length <= 0 ? []
+        : req.files.map(function (file) {
+          return { name: file.filename, description: 'add prod' }//galleryschema:name=nom de photo,description=add prod
+        })
+
+    const lodge = new Lodge(req.body)
+    await lodge.save()
+    await Place.findByIdAndUpdate({
+      _id: req.body.place
+    }, { $push: { lodges: lodge } })
+    res.status(200).json({
+      data: lodge,
+      msg: 'lodge created successfully'
+    })
+  }
+  catch (error) {
+    res.status(404).json({
+      msg: 'error' + error.message
+    })
+  }
 }
 getAllLodges = async (req, res) => {
   const allLodges = await Lodge.find({})
@@ -28,6 +45,9 @@ updateLodge = async (req, res) => {
   res.status(200).json({ msg: 'lodge updated' })
 }
 deleteLodge = async (req, res) => {
+  await Lodge.findByIdAndUpdate(Lodge.place, {
+    $pull: { lodges: req.params.id },
+  });
   const deletedLodge = await Lodge.findOneAndRemove({ _id: req.params.id })
   res.status(200).json({
     msg: 'deleted successfully '
