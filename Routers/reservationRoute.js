13e0f20254bf11822d199ef2dd93680bb8276da9
Amine Khmissi <khmissiamine111@gmail.com>
2022-11-14 15:31:06 +0100

    test

--- Routers/reservationRoute.js
+++ Routers/reservationRoute.js
@@ -7,5 +7,6 @@ route.get("/getAllReservation", reservController.getAllReservation);
 route.get('/confirmReservation/:confCode',reservController.confirmReservation)
 route.get('/getReservationById/:id',reservController.getReservationById)
 route.delete('/deleteReservation/:id',reservController.deleteReservation)
+route.get('/getAllReservation',reservController.getAllReservation)
 
 module.exports = route;
