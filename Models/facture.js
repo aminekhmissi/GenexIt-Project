12824cb5d2a294d719fb032351363d0c315ec479
karimerrebai <karimerrebai@gmail.com>
2022-11-15 10:01:09 +0100

    now

--- Models/facture.js
+++ Models/facture.js
@@ -3,8 +3,14 @@ const schemaFacture = new mongoose.Schema({
   ref: {
     type: String
   },
+  reservation: {
+    type: mongoose.Types.ObjectId,
+    require: false
+  },
   discount: {
-    type: String  }
+    type: String,
+    require: false
+  }
 }, { timestamps: true }
 )
 module.exports = mongoose.model('Facture', schemaFacture)
